ef3a0d045c3880232710194094e7f336e8520a43
cesar2372 <111533266+cesar2372@users.noreply.github.com>
2022-09-08 16:05:12 -0600

    Add files via upload

---
+++ division)else).py
@@ -0,0 +1,7 @@
+n1=int(input('ingrese el primer numero de la division: '))
+n2=int(input('ingrese el segundo numero de la division: '))
+resultado=n1/n2
+if resultado>0 :
+ print (resultado)
+else:
+    print ('error')
